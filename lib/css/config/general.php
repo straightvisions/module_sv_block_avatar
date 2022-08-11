af03663488d2e76fef0594c5f0310386518e113f
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-08-11 17:44:07 +0200

    init

---
+++ lib/css/config/general.php
@@ -0,0 +1,9 @@
+<?php
+	echo $_s->build_css(
+		'.wp-block-avatar',
+		array_merge(
+			$module->get_setting('padding')->get_css_data('padding'),
+			$module->get_setting('margin')->get_css_data(),
+			$module->get_setting('border')->get_css_data()
+		)
+	);
